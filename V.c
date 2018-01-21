c68b0b377653ffa11469a8b2ef4c4567bebf9e09
Rajmohanmech <35652533+Rajmohanmech@users.noreply.github.com>
2018-01-21 16:50:25 +0530

    Create V.c

---
+++ V.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+void main()
+{
+char A,a,e,i,o,u:
+printf("ENTER THE ALPHABETS");
+scanf("%c",&A);
+if(A=a,e,i,o,u)
+{
+printf(" \n CONSONANTS");
+}
+else
+{
+printf(" \n VOWELS");
+}
+}
